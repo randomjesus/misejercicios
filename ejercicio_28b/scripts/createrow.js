3739d6347230d55c02098fa4e731510c8e6805b5
Jesus Sierralaya <random.music.1234567890@gmail.com>
2024-07-23 17:04:46 +0200

    Adds key description

--- ejercicio_28b/scripts/createrow.js
+++ ejercicio_28b/scripts/createrow.js
@@ -1,32 +1,37 @@
-  function createRow(image, name, title) {
-    // class champion
-    let divChampion = document.createElement("div");
-    divChampion.classList.add("champion");
-    document.querySelector("#champions").appendChild(divChampion);
-    // class image
-    let divImage = document.createElement("div");
-    divImage.classList.add("image");
-    divChampion.appendChild(divImage);
-    // img src
-    let imgChampion = document.createElement("img");
-    imgChampion.src = image;
-    divImage.appendChild(imgChampion);
-    // class description
-    let divDescription = document.createElement("div");
-    divDescription.classList.add("description");
-    divChampion.appendChild(divDescription);
-    //   <div class="name">Aatrox</div>
-    let divName = document.createElement("div");
-    divName.classList.add("name");
-    divName.appendChild(document.createTextNode(name));
-    divDescription.appendChild(divName);
-    //   <div class="title">the Darkin Blade</div>
-    let divTitle = document.createElement("div");
-    divTitle.classList.add("title");
-    divTitle.appendChild(document.createTextNode(title));
-    divDescription.appendChild(divTitle);
-    //hr
-    // document
-    //   .querySelector("#champions")
-    //   .appendChild(document.createElement("hr"));
-  }
+function createRow(image, name, title, key) {
+  // class champion
+  let divChampion = document.createElement("div");
+  divChampion.classList.add("champion");
+  document.querySelector("#champions").appendChild(divChampion);
+  // class image
+  let divImage = document.createElement("div");
+  divImage.classList.add("image");
+  divChampion.appendChild(divImage);
+  // img src
+  let imgChampion = document.createElement("img");
+  imgChampion.src = image;
+  divImage.appendChild(imgChampion);
+  // class description
+  let divDescription = document.createElement("div");
+  divDescription.classList.add("description");
+  divChampion.appendChild(divDescription);
+  //   <div class="name">Aatrox</div>
+  let divName = document.createElement("div");
+  divName.classList.add("name");
+  divName.appendChild(document.createTextNode(name));
+  divDescription.appendChild(divName);
+  //   <div class="title">the Darkin Blade</div>
+  let divTitle = document.createElement("div");
+  divTitle.classList.add("title");
+  divTitle.appendChild(document.createTextNode(title));
+  divDescription.appendChild(divTitle);
+  //   <div class="key">266</div>
+  let divKey = document.createElement("div");
+  divKey.classList.add("key");
+  divKey.appendChild(document.createTextNode(`Key: ${key}`));
+  divDescription.appendChild(divKey);
+  //hr
+  // document
+  //   .querySelector("#champions")
+  //   .appendChild(document.createElement("hr"));
+}

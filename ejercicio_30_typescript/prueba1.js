8f1cddb3ad5bdad39aa4a3eaff264a9b00b4aeea
Jesus Sierralaya <random.music.1234567890@gmail.com>
2024-07-23 20:48:18 +0200

    Adds ts first contact

---
+++ ejercicio_30_typescript/prueba1.js
@@ -0,0 +1,14 @@
+var edad = 10;
+var nombre = "Jesus";
+function sumar(n1, n2) {
+    return n1 + n2;
+}
+var x = null;
+console.log(typeof x);
+var Factura = /** @class */ (function () {
+    function Factura(numero) {
+        this.numero = numero;
+    }
+    return Factura;
+}());
+var f1 = new Factura(1000);
---
+++ ejercicio_30_typescript/prueba1.ts
@@ -0,0 +1,16 @@
+let edad: number = 10;
+let nombre: string = "Jesus";
+
+function sumar(n1: number, n2: number): number {
+  return n1 + n2;
+}
+let x: string = null;
+console.log(typeof x);
+
+class Factura {
+  numero: number;
+  constructor(numero: number) {
+    this.numero = numero;
+  }
+}
+let f1 = new Factura(1000);
